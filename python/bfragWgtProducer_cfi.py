2e1c44115bb96762e7d502e7d1a1bd5594278e2d
Sebastien Wertz <sebastien.wertz@cern.ch>
2021-02-09 08:38:54 +0100

    activate only recommended weights per default

--- python/bfragWgtProducer_cfi.py
+++ python/bfragWgtProducer_cfi.py
@@ -6,17 +6,18 @@ bfragWgtProducer = cms.EDProducer('BFragmentationWeightProducer',
                                   br_weights = cms.vstring(["semilepbrup", "semilepbrdown"]),
                                   frag_weight_file = cms.FileInPath('TopQuarkAnalysis/BFragmentationAnalyzer/data/bfragweights.root'),
                                   frag_weights = cms.vstring([
-                                        "fragCP5BL", # CP5, Bowler-Lund, tuned to LEP + up/down uncertainties
-                                        "fragCP5BLdown",
-                                        "fragCP5BLup",
-                                        "fragCP5Peterson", # CP5, Peterson, tuned to LEP + up/down uncertainties
-                                        "fragCP5Petersondown",
-                                        "fragCP5Petersonup",
-                                        "fragCUETP8M2T4BL", # CUETP8M2T4, Bowler-Lund, tuned to LEP (assuming Monash)
-                                        "fragCUETP8M2T4BLdefault", # CUETP8M2T4, Bowler-Lund, Pythia8 default
-                                        "fragCUETP8M2T4BLLHC", # CUETP8M2T4, Bowler-Lund, TOP-18-012 result + up/down uncertainties
-                                        "fragCUETP8M2T4BLLHCdown",
-                                        "fragCUETP8M2T4BLLHCup",
+                                        # pt-averaged weights provided but not recommended
+                                        # "fragCP5BL", # CP5, Bowler-Lund, tuned to LEP + up/down uncertainties
+                                        # "fragCP5BLdown",
+                                        # "fragCP5BLup",
+                                        # "fragCP5Peterson", # CP5, Peterson, tuned to LEP + up/down uncertainties
+                                        # "fragCP5Petersondown",
+                                        # "fragCP5Petersonup",
+                                        # "fragCUETP8M2T4BL", # CUETP8M2T4, Bowler-Lund, tuned to LEP (assuming Monash)
+                                        # "fragCUETP8M2T4BLdefault", # CUETP8M2T4, Bowler-Lund, Pythia8 default
+                                        # "fragCUETP8M2T4BLLHC", # CUETP8M2T4, Bowler-Lund, TOP-18-012 result + up/down uncertainties
+                                        # "fragCUETP8M2T4BLLHCdown",
+                                        # "fragCUETP8M2T4BLLHCup",
                                   ]),
                                   frag_weight_vs_pt_file = cms.FileInPath('TopQuarkAnalysis/BFragmentationAnalyzer/data/bfragweights_vs_pt.root'),
                                   frag_weights_vs_pt = cms.vstring([
@@ -26,10 +27,11 @@ bfragWgtProducer = cms.EDProducer('BFragmentationWeightProducer',
                                         "fragCP5Peterson", # CP5, Peterson, tuned to LEP + up/down uncertainties
                                         "fragCP5Petersondown",
                                         "fragCP5Petersonup",
-                                        "fragCUETP8M2T4BL", # CUETP8M2T4, Bowler-Lund, tuned to LEP (assuming Monash)
-                                        "fragCUETP8M2T4BLdefault", # CUETP8M2T4, Bowler-Lund, Pythia8 default
-                                        "fragCUETP8M2T4BLLHC", # CUETP8M2T4, Bowler-Lund, TOP-18-012 result + up/down uncertainties
-                                        "fragCUETP8M2T4BLLHCdown",
-                                        "fragCUETP8M2T4BLLHCup",
+                                        # alternative weights for old tune provided but not recommended
+                                        # "fragCUETP8M2T4BL", # CUETP8M2T4, Bowler-Lund, tuned to LEP (assuming Monash)
+                                        # "fragCUETP8M2T4BLdefault", # CUETP8M2T4, Bowler-Lund, Pythia8 default
+                                        # "fragCUETP8M2T4BLLHC", # CUETP8M2T4, Bowler-Lund, TOP-18-012 result + up/down uncertainties
+                                        # "fragCUETP8M2T4BLLHCdown",
+                                        # "fragCUETP8M2T4BLLHCup",
                                   ]),
                                   )
